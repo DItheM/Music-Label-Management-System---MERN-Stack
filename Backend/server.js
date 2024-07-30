03f3c4622a07f7aa256321176414f46b6a0ee13d
DItheM <d.ipalawatta@gmail.com>
2024-07-30 20:45:37 +0530

    .env added

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const express = require("express")
 
 
@@ -5,6 +7,6 @@ const express = require("express")
 const app =express()
 
 // listen for requests
-app.listen(5000, () => {
-    console.log("Listening on port 5000")
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port", process.env.PORT)
 })
